ec5dfb0cfb4abce0e51a18b6ccf2c2b027063825
Jiří Strnadel <Juzba88@gmail.com>
2025-04-30 05:08:21 +0200

    update

--- ConsoleEntity/Program.cs
+++ ConsoleEntity/Program.cs
@@ -9,43 +9,45 @@ internal class Program
 
         Console.WriteLine("Start programu");
 
-        do
+        using (var context = new MyDbContext())
         {
-
-
-            ApiResponse? apiResponse = new();
-
-            using (HttpClient client = new HttpClient())
+            do
             {
-                try
+                ApiResponse? apiResponse = new();
+                using (HttpClient client = new HttpClient())
                 {
-                    HttpResponseMessage response = await client.GetAsync("http://api.open-notify.org/iss-now.json");
-                    response.EnsureSuccessStatusCode(); // Zajistí, že HTTP odpověď bude 200-299  
-                    string responseBody = await response.Content.ReadAsStringAsync();
-                    apiResponse = JsonConvert.DeserializeObject<ApiResponse>(responseBody);
-                    //Console.WriteLine(responseBody);
+                    try
+                    {
+                        HttpResponseMessage response = await client.GetAsync("http://api.open-notify.org/iss-now.json");
+                        response.EnsureSuccessStatusCode(); // Zajistí, že HTTP odpověď bude 200-299  
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        apiResponse = JsonConvert.DeserializeObject<ApiResponse>(responseBody);
+                        //Console.WriteLine(responseBody);
 
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine($"Chyba při získávání dat: {e.Message}");
+                    }
                 }
-                catch (HttpRequestException e)
-                {
-                    Console.WriteLine($"Chyba při získávání dat: {e.Message}");
-                }
-            }
 
-            if (apiResponse?.Message == "success")
-            {
-                using (var context = new MyDbContext())
+                if (apiResponse?.Message == "success")
                 {
                     var issDbPossition = new IssDbPosition() { ID = 0, Iss_Position = apiResponse.Iss_Position, Message = apiResponse.Message, Timestamp = apiResponse.Timestamp };
 
                     context.Add(issDbPossition);
-                    context.SaveChanges();
-
-
-                    Console.WriteLine("Success!!");
+                    Console.WriteLine($"Success!! {count}");
                 }
-            }
-        } while (count++ < 10);
+
+                await Task.Delay(2000);
+            } while (count++ < 10);
+
+            context.SaveChanges();
+            Console.WriteLine("Konec");
+
+
+
+        }
     }
 
 
